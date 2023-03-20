f17d60fc6bae7b2d7491d8d3d477df3ce80c15ec
AndreaGarofalo <andreasjobs94@outlook.it>
2023-03-20 17:18:06 +0100

    working on cards detail

--- src/router/index.js
+++ src/router/index.js
@@ -5,28 +5,27 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import HomePage from "../pages/HomePage.vue";
 
-import NotFoundPage from '../pages/NotFoundPage.vue';
+import GameDetailPage from "../pages/GameDetailPage.vue";
+import NotFoundPage from "../pages/NotFoundPage.vue";
 
 // Creo il Router e definisco le rotte
 const router = createRouter({
+  // History delle pagine
+  history: createWebHistory(),
+  // Importo la classe da avere in active
+  linkActiveClass: "active",
 
-    // History delle pagine
-    history: createWebHistory(),
-    // Importo la classe da avere in active
-    linkActiveClass: "active",
+  // Rotte che useremo
+  routes: [
+    { path: "/", name: "home", component: HomePage },
 
-    // Rotte che useremo
-    routes: [
-        { path: "/", name: "home", component: HomePage },
+    // TODO { path: '/contact', name: 'contact', component: ContactPage },
 
-        // TODO { path: '/contact', name: 'contact', component: ContactPage },
+    //Rotta per lo show del game con ID
+    { path: "/games/:id", name: "game-detail", component: GameDetailPage },
 
-        //Rotta per lo show del game con ID
-        //  TODO { path: '/games/:id', name: 'game-detail', component: GameDetailPage },
-
-        { path: "/:pathMatch(.*)*", name: "not-found", component: NotFoundPage },
-
-    ],
+    { path: "/:pathMatch(.*)*", name: "not-found", component: NotFoundPage },
+  ],
 });
 
 export { router };
