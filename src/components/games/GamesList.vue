a71c7bdde6479fc19537721a56909a421e81a8c4
ArmandoMarino <scrivania.lavoro@gmail.com>
2023-03-20 15:00:03 +0100

    gamecard, gamelist and router

---
+++ src/components/games/GamesList.vue
@@ -0,0 +1,19 @@
+<script>
+import GameCard from './GameCard.vue';
+export default {
+    name: 'GamesList',
+    components: { GameCard },
+    props: { games: Array }
+}
+</script>
+
+<template>
+    <section id="games-list" class="my-4">
+        <div v-if="games.length">
+            <game-card v-for="project in games" :key="game.id" :game="game">
+                {{ game.title }}
+            </game-card>
+        </div>
+        <h3 v-else class="text-center">No games available</h3>
+    </section>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -9,16 +9,20 @@ import HomePage from "../pages/HomePage.vue";
 
 // Creo il Router e definisco le rotte
 const router = createRouter({
-  // History delle pagine
-  history: createWebHistory(),
-  // Importo la classe da avere in active
-  linkActiveClass: "active",
+    // History delle pagine
+    history: createWebHistory(),
+    // Importo la classe da avere in active
+    linkActiveClass: "active",
 
-  // Rotte che useremo
-  routes: [
-    { path: "/", name: "home", component: HomePage },
-    // { path: '/contact', name: 'contact', component: ContactPage },
-  ],
+    // Rotte che useremo
+    routes: [
+        { path: "/", name: "home", component: HomePage },
+
+        // TODO { path: '/contact', name: 'contact', component: ContactPage },
+
+        //Rotta per lo show del Project con ID
+        // TODO { path: '/games/:id', name: 'project-detail', component: GameDetailPage },
+    ],
 });
 
 export { router };
