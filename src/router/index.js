efe51e5da0d9518a7c10f8de09379d2eeb72a18e
AndreaGarofalo <andreasjobs94@outlook.it>
2023-03-20 15:04:54 +0100

    added path in index.js

--- src/router/index.js
+++ src/router/index.js
@@ -18,6 +18,7 @@ const router = createRouter({
   routes: [
     { path: "/", name: "home", component: HomePage },
     // { path: '/contact', name: 'contact', component: ContactPage },
+    { path: "/:pathMatch(.*)*", name: "not-found", component: NotFoundPage },
   ],
 });
 
