0d239b4ee29ffb1e23b2ae6856b40220c00eb8e2
ArmandoMarino <scrivania.lavoro@gmail.com>
2023-03-21 17:14:06 +0100

    card update markup

--- src/components/games/GameCard.vue
+++ src/components/games/GameCard.vue
@@ -37,7 +37,7 @@ export default {
             return `${day}/${month}/${year} - ${hours}:${minutes}:${seconds} `;
         },
         abstractDescription() {
-            const abstract = this.game.description.slice(0, 150);
+            const abstract = this.game.description.slice(0, 70);
             return abstract + "...";
         },
     },
@@ -46,43 +46,50 @@ export default {
 
 <template>
     <div class="my-4 text-center" :class="isDetail ? 'col-11 mx-auto my-card-detail' : 'col-3'">
-        <div class="p-3 card h-100" :class="isDetail ? 'my-card-detail' : 'my-card-list'">
-            <!-- :style="{ backgroundImage: 'url(' + game.image + ')' }" -->
-            <!--* IMAGE -->
-            <!-- <img v-if="game.image" :src="game.image" class="card-img-top img-fluid" :alt="game.title"> -->
-            <div class="card-body d-flex flex-column justify-content-between">
-                <h5 class="card-title">{{ game.title }}</h5>
 
-                <!--TODO EDITOR -->
-                <!-- da router  il v-if qui è MOLTO importante -->
-                <!-- <router-link v-if="game.editor" :to="{ name: 'editor-games', params: { id: game.editor.id } }">
-                                                                                                                                                                                                                                                    <span class="badge" :style="{ backgroundColor: game.editor.color }">
-                                                                                                                                                                                                                                                        {{ game.editor.label }}
-                                                                                                                                                                                                                                                    </span>
-                                                                                                                                                                                                                                                </router-link> -->
+        <!-- ROUTER -->
+        <router-link v-if="!isDetail" :to="{ name: 'game-detail', params: { id: game.id } }">
 
-                <!--*TODO metterla nello show ABSTRACT | DESCRIPTION -->
-                <p class="card-text">
-                    {{ isDetail ? game.description : abstractDescription }}
-                </p>
+            <div class="p-3 card h-100" :class="isDetail ? 'my-card-detail' : 'my-card-list'"
+                :style="{ backgroundImage: 'url(' + game.image + ')' }">
+                <div class=" h-100 overlay">
+                    <!--* IMAGE -->
+                    <!-- <img v-if="game.image" :src="game.image" class="card-img-top img-fluid" :alt="game.title"> -->
+                    <div class="card-body d-flex flex-column justify-content-between">
+                        <h5 class="card-title">{{ game.title }}</h5>
 
-                <!--* CARD FOOTER -->
-                <div class="card-footer">
-                    <!-- UPDATED & PUBLISHED -->
-                    <p>Pubblished at : {{ gameDateCreated }}</p>
-                    <p>Updated at : {{ gameDateUpdated }}</p>
-                    <!-- GENRES -->
-                    <span v-for="genre in game.genres" :class="`text-bg-${genre.color}`" :key="genre.id" class="badge">
-                        {{ genre.label }}
-                    </span>
-                    <!--TODO BUTTON TO DETAILS inglobiamo tutto come overlay and hover per lo show -->
-                    <router-link v-if="!isDetail" :to="{ name: 'game-detail', params: { id: game.id } }"
-                        class="btn btn-primary">
-                        Show details
-                    </router-link>
+                        <!--TODO EDITOR -->
+                        <!-- da router  il v-if qui è MOLTO importante -->
+                        <!-- <router-link v-if="game.editor" :to="{ name: 'editor-games', params: { id: game.editor.id } }">
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        <span class="badge" :style="{ backgroundColor: game.editor.color }">
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            {{ game.editor.label }}
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        </span>
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    </router-link> -->
+
+                        <!--*TODO metterla nello show ABSTRACT | DESCRIPTION -->
+                        <p class="card-text">
+                            {{ isDetail ? game.description : abstractDescription }}
+                        </p>
+
+                        <!--* CARD FOOTER -->
+                        <div v-if="isDetail" class="card-footer">
+                            <!-- UPDATED & PUBLISHED -->
+                            <p>Pubblished at : {{ gameDateCreated }}</p>
+                            <p>Updated at : {{ gameDateUpdated }}</p>
+                            <!-- GENRES -->
+
+                        </div>
+                        <div class="d-flex fs-6">
+                            <span v-for="genre in game.genres" :style="{ backgroundColor: genre.color }" :key="genre.id"
+                                class="badge">
+                                {{ genre.label }}
+                            </span>
+                        </div>
+
+                    </div>
                 </div>
             </div>
-        </div>
+        </router-link>
     </div>
 </template>
 
@@ -116,7 +123,7 @@ body {
 .card {
     background-position: center;
     background-repeat: no-repeat;
-    background-size: cover;
+    background-size: contain;
     width: var(--card-width);
     height: var(--card-height);
     padding: 3px;
@@ -129,13 +136,19 @@ body {
     font-size: 1.5em;
     color: rgb(88 199 250 / 0%);
     cursor: pointer;
+    min-height: 300px;
     // font-family: cursive;
 }
 
 .card:hover {
     color: white;
     transition: color 1.4s;
-    background-color: rgba(0, 0, 0, 0.3);
+}
+
+.card:hover .overlay {
+    border-radius: 25px;
+    background-color: rgba(0, 0, 0, 0.2);
+    text-shadow: black 1px 2px;
 }
 
 .card:hover:before,
