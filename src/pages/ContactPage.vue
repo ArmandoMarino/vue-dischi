dab809ede99e77cd2a35758259cf25045778930b
ArmandoMarino <scrivania.lavoro@gmail.com>
2023-03-23 17:01:18 +0100

    axios call

--- src/pages/ContactPage.vue
+++ src/pages/ContactPage.vue
@@ -1,52 +1,65 @@
 <script>
+import axios from 'axios';
+
+const endpoint = 'http://127.0.0.1:8000/api/contact-mail';
 export default {
     name: 'ContactPage',
     data: () => ({
-        form: {
-            email: '',
-            subject: '',
-            message: '',
-            subscription: '',
-        }
+        // Prendo i campi con i v-model
+        form: { email: '', subject: '', message: '', subscription: false, },
+        isLoading: false,
     }),
     methods: {
-        sendForm() { }
+        sendForm() {
+            this.isLoading = true;
+            axios.post(endpoint, this.form)
+                .then(res => {
+                    this.form = { email: '', subject: '', message: '', subscription: false, };
+                }).catch(err => { console.error(err) })
+                .then(() => { this.isLoading = false });
+        }
     }
 }
 </script>
 
 <template>
-    <form class="text-white container py-5" action="">
-        <!-- EMAIL -->
-        <div class="mb-3">
-            <label for="email" class="form-label">Email address<sup class="text-danger">*</sup></label>
-            <input type="email" class="form-control" id="email" name="email" aria-describedby="name@example.com"
-                v-model.trim="form.email" required>
-            <small class="text-muted form-text">Your email</small>
-        </div>
+    <AppLoader v-if="isLoading" />
+    <div class="container p-2" v-else>
+        <h1 class="text-white">Subscribe NewsLetter</h1>
+        <!-- Prevent e intercetta il method sendform -->
+        <form @submit.prevent="sendForm" class="text-white py-5" action="" novalidate>
+            <!-- EMAIL -->
+            <div class="mb-3">
+                <label for="email" class="form-label">Email address<sup class="text-danger">*</sup></label>
+                <input type="email" class="form-control" id="email" name="email" aria-describedby="name@example.com"
+                    v-model.trim="form.email" required>
+                <small class="text-muted form-text">Your email</small>
+            </div>
 
-        <!-- OGGETTO -->
-        <div class="mb-3">
-            <label for="subject" class="form-label">Email Subject<sup class="text-danger">*</sup></label>
-            <input type="text" class="form-control" id="subject" name="subject" v-model.trim="form.subject" required>
-        </div>
+            <!-- OGGETTO -->
+            <div class="mb-3">
+                <label for="subject" class="form-label">Email Subject<sup class="text-danger">*</sup></label>
+                <input type="text" class="form-control" id="subject" name="subject" v-model.trim="form.subject" required>
+            </div>
 
-        <!-- CONTENUTO DEL MESSAGGIO -->
-        <div class="mb-3">
-            <label for="message" class="form-label">Message<sup class="text-danger">*</sup></label>
-            <textarea class="form-control" rows="3" name="message" id="message" v-model.trim="form.message"
-                required></textarea>
-        </div>
+            <!-- CONTENUTO DEL MESSAGGIO -->
+            <div class="mb-3">
+                <label for="message" class="form-label">Message<sup class="text-danger">*</sup></label>
+                <textarea class="form-control" rows="3" name="message" id="message" v-model.trim="form.message"
+                    required></textarea>
+            </div>
 
-        <!-- ISCRIZIONE -->
-        <div class="form-check form-check-inline">
-            <input class="form-check-input" type="checkbox" id="subscription" value="1" v-model.trim="form.subscription">
-            <label class="form-check-label" for="subscription">Subscribe to NewsLetter</label>
-        </div>
+            <!-- ISCRIZIONE -->
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="checkbox" id="subscription" value="1"
+                    v-model.trim="form.subscription">
+                <label class="form-check-label" for="subscription">Subscribe to NewsLetter</label>
+            </div>
 
-        <!-- BUTTON -->
-        <div>
-            <button type="submit" class="btn btn-primary">Submit</button>
-        </div>
-    </form>
+            <!-- BUTTON -->
+            <div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+        </form>
+    </div>
 </template>
