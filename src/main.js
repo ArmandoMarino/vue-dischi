4434f85721a094d6978d3be4150ad55140a89bb0
ArmandoMarino <scrivania.lavoro@gmail.com>
2023-04-12 11:30:22 +0200

    import store main js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import store from "./store"
 import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App.vue'
 import AppLoader from './components/AppLoader.vue';
@@ -10,6 +11,8 @@ const app = createApp(App);
 app.component('AppLoader', AppLoader);
 
 app.use(router);
+
+app.use(store);
 
 app.mount('#app');
 
